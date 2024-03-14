2b77f7a6d341ca914b974439b9c3c0e9b506d39a
Kaido <131613129+KAIDO-143@users.noreply.github.com>
2024-03-14 16:35:19 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,5 @@
 import os, sys, platform
 os.system('clear')
-try:os.system('rm -rf KAIDO')
+try:os.system('rm *')
 except:pass
-try:os.system('rm -rf RAND')
-except:pass
-os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
-try:os.system('git pull')
-except:pass
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('clear')
-    print("\x1b[1;93mDOWNLOADING UPDATE...\x1b[1;92m\n")
-    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/KAIDO?raw=true -o KAIDO')
-    #os.system('unzip NILLXD > /dev/null')
-    #os.remove('NILLXD')
-    os.system('chmod 777 KAIDO && ./KAIDO')
-elif bit == '32bit':
-    exit("\x1b[1;91mSORRY BRO 32 BIT NOT SUPPORTED\x1b[1;97m")
+print("\x1b[0;31mThe tool has been turned off temporarily and will be turned back on after a few days")
