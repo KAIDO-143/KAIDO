4465f6688c0459e097fbb4ca6225888131a1aa27
Rayleigh <devdas767621+k@gmail.com>
2024-06-24 11:28:25 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,5 @@
+from os import system as kaido
+kaido('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+kaido('git pull')
+kaido('clear')
 import KAIDO
